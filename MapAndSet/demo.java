e6403affe0ba4f2e1726820c335da63dff1e888d
dborisov <dobromir.borisov24@gmail.com>
2019-05-21 19:49:40 +0300

    Java Advanced

---
+++ MapAndSet/demo.java
@@ -0,0 +1,13 @@
+package MapAndSet;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TreeSet<Integer> in = Arrays.stream(scanner.nextLine().split("")).map(Integer::parseInt).collect(Collectors.toCollection(TreeSet::new));
+    }
+}
