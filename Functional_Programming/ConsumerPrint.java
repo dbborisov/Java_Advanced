493b258f509217f200dec8b0ba3442f0351d505a
dborisov <dobromir.borisov24@gmail.com>
2019-06-04 11:15:15 +0300

    Java Advanced

--- Functional_Programming/ConsumerPrint.java
+++ Functional_Programming/ConsumerPrint.java
@@ -1,5 +1,22 @@
 package Functional_Programming;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class ConsumerPrint {
+    public static void main(String[] args) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            Arrays.stream(reader.readLine().split("\\s+")).forEach(x-> System.out.println(x));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 }
